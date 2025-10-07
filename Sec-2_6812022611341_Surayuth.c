ab4afc8ca215b93ef2e174b30660dd1d32904158
surayuthpanngom <s6812022611341@email.kmutnb.ac.th>
2025-10-07 15:24:46 +0700

    Add files via upload

---
+++ Sec-2_6812022611341_Surayuth.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int time ;
+    float base_fee,toal_fee;
+    char service;
+
+
+    printf("ประเภทช่องทางธุรกรรม:\n")
+    printf("ประเภท O ออนไลน์ = 15บาท \n")
+    printf("ประเภท ที่ Bสาขา = 40 บาท \n")
+    printf("ประเภท A ตู้ ATM  20 บาท\n")
+}
+    printf("กรอกประภทของอัตราค่าธรรมเนียม O,B,A): ")
+    scanf ("%c", &service);
+
+    printf("กรอกจำนวนระหว่าง3-12"): ");
+    scanf("%d" , &time);
+  
+    if (times < 3 , times >12) {
+    printf("จำนวนครั้งที่ต้องอยู่ระหว่าง 3-12 \n");
+    return 0;
+}
+
+
+
+
+
+    
